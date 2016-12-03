731825e7fea3ab71e63ecea3b2e5ad015c5330d3
Suraj Nair <suraj.jayraman@gmail.com>
2016-12-03 12:12:54 +0530

    MediaPlayer Implementation

---
+++ app/src/main/java/ielite/app/musicplayer/MainActivity.java
@@ -0,0 +1,38 @@
+package ielite.app.musicplayer;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.sriramana);
+
+        Button playButton = (Button) findViewById(R.id.play_button);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.start();
+            }
+        });
+
+        Button pauseButton = (Button) findViewById(R.id.pause_button);
+
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.pause();
+            }
+        });
+    }
+}
